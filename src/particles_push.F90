2a9525b092208df2052e2cd1e9d7bd3219f03ebb
Jean-Luc Vay <jlvay@jlvay2.lbl.gov>
2016-01-15 08:36:33 -0800

    A few fixes and addition of split versions.

--- src/particles_push.F90
+++ src/particles_push.F90
@@ -54,6 +54,11 @@ DO iz=1, ntilez ! LOOP ON TILES
                     np=MIN(count-ip+1,nblk)
                     ipmin=ip
                     ipmax=ip+np-1
+                    write(0,*) '#1',curr_tile%x_grid_tile_min,curr_tile%y_grid_tile_min,                             &
+                                      curr_tile%z_grid_tile_min, dx,dy,dz,curr_tile%nx_cells_tile,                     &
+                                      curr_tile%ny_cells_tile,curr_tile%nz_cells_tile,nxjguards,nyjguards
+                    write(0,*)  '#2',jmin,jmax,kmin,kmax,lmin,lmax,ip,np,ipmin,ipmax
+                    write(0,*) 'max ey',maxval(ey)
                     !!! --- Gather electric field on particles
                     CALL gete3d_energy_conserving_1_1_1(np,curr_tile%part_x(ipmin:ipmax),curr_tile%part_y(ipmin:ipmax),&
                                       curr_tile%part_z(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax),                   &
@@ -72,18 +77,19 @@ DO iz=1, ntilez ! LOOP ON TILES
                                       curr_tile%ny_cells_tile,curr_tile%nz_cells_tile,nxjguards,nyjguards,                &
                                       nzjguards,bx(jmin:jmax,kmin:kmax,lmin:lmax),by(jmin:jmax,kmin:kmax,lmin:lmax),      &
                                       bz(jmin:jmax,kmin:kmax,lmin:lmax))
-                    !!! --- Push velocity with B half step
-                    CALL bpush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
-                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_bx(ipmin:ipmax), curr_tile%part_by(ipmin:ipmax), &
-                    curr_tile%part_bz(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    write(0,*) 'max eyp',maxval(curr_tile%part_ey(ipmin:ipmax))
                     !!! --- Push velocity with E half step
                     CALL epush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
                     curr_tile%part_uz(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax), curr_tile%part_ey(ipmin:ipmax), &
-                    curr_tile%part_ez(ipmin:ipmax), curr%charge,curr%mass,dt)
+                    curr_tile%part_ez(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
                     !!! --- Push velocity with B half step
                     CALL bpush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
                     curr_tile%part_uz(ipmin:ipmax), curr_tile%part_bx(ipmin:ipmax), curr_tile%part_by(ipmin:ipmax), &
-                    curr_tile%part_bz(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    curr_tile%part_bz(ipmin:ipmax), curr%charge,curr%mass,dt)
+                    !!! --- Push velocity with E half step
+                    CALL epush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
+                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax), curr_tile%part_ey(ipmin:ipmax), &
+                    curr_tile%part_ez(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
                     !!!! --- push particle species positions a time step
                     CALL pushxyz(np,curr_tile%part_x(ipmin:ipmax),curr_tile%part_y(ipmin:ipmax),                    &
                     curr_tile%part_z(ipmin:ipmax), curr_tile%part_ux(ipmin:ipmax),curr_tile%part_uy(ipmin:ipmax),   &
@@ -98,6 +104,160 @@ tend=MPI_WTIME()
 pushtime=pushtime+(tend-tdeb)
 END SUBROUTINE push_particles
 
+SUBROUTINE pxrpush_particles_part2
+USE particles
+USE constants
+USE fields
+USE params
+USE shared_data
+USE tiling
+IMPLICIT NONE
+INTEGER(idp) :: ispecies, ix, iy, iz, count
+INTEGER(idp) :: jmin, jmax, kmin, kmax, lmin, lmax
+TYPE(particle_species), POINTER :: curr
+TYPE(particle_tile), POINTER :: curr_tile
+REAL(num) :: tdeb, tend
+INTEGER(idp) :: nxc, nyc, nzc, ipmin,ipmax, np,ip
+INTEGER(idp) :: nblk=1000000
+
+tdeb=MPI_WTIME()
+!$OMP PARALLEL DO COLLAPSE(3) SCHEDULE(runtime) DEFAULT(NONE) &
+!$OMP SHARED(ntilex,ntiley,ntilez,nspecies,species_parray, &
+!$OMP nxjguards,nyjguards,nzjguards,ex,ey,ez,bx,by,bz,dx,dy,dz,dt,nblk) &
+!$OMP PRIVATE(ix,iy,iz,ispecies,curr,curr_tile,count,jmin,jmax,kmin,kmax,lmin, &
+!$OMP lmax,nxc,nyc,nzc, ipmin,ipmax,ip,np)
+DO iz=1, ntilez ! LOOP ON TILES
+    DO iy=1, ntiley
+        DO ix=1, ntilex
+            DO ispecies=1, nspecies ! LOOP ON SPECIES
+                ! - Get current tile properties
+                ! - Init current tile variables
+                curr=>species_parray(ispecies)
+                curr_tile=>curr%array_of_tiles(ix,iy,iz)
+                count=curr_tile%np_tile
+                jmin=curr_tile%nx_tile_min-nxjguards
+                jmax=curr_tile%nx_tile_max+nxjguards
+                kmin=curr_tile%ny_tile_min-nyjguards
+                kmax=curr_tile%ny_tile_max+nyjguards
+                lmin=curr_tile%nz_tile_min-nzjguards
+                lmax=curr_tile%nz_tile_max+nzjguards
+                nxc=curr_tile%nx_cells_tile
+                nyc=curr_tile%ny_cells_tile
+                nzc=curr_tile%nz_cells_tile
+                !!! ---- Loop by blocks over particles in a tile (blocking)
+                DO ip=1,count,nblk
+                    np=MIN(count-ip+1,nblk)
+                    ipmin=ip
+                    ipmax=ip+np-1
+                    !!! --- Push velocity with B half step
+                    CALL bpush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
+                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_bx(ipmin:ipmax), curr_tile%part_by(ipmin:ipmax), &
+                    curr_tile%part_bz(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    !!! --- Push velocity with E half step
+                    CALL epush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
+                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax), curr_tile%part_ey(ipmin:ipmax), &
+                    curr_tile%part_ez(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    !!!! --- push particle species positions a time step
+                    CALL pushxyz(np,curr_tile%part_x(ipmin:ipmax),curr_tile%part_y(ipmin:ipmax),                    &
+                    curr_tile%part_z(ipmin:ipmax), curr_tile%part_ux(ipmin:ipmax),curr_tile%part_uy(ipmin:ipmax),   &
+                    curr_tile%part_uz(ipmin:ipmax),dt)
+                END DO
+            END DO! END LOOP ON SPECIES
+        END DO
+    END DO
+END DO! END LOOP ON TILES
+!$OMP END PARALLEL DO
+tend=MPI_WTIME()
+pushtime=pushtime+(tend-tdeb)
+END SUBROUTINE pxrpush_particles_part2
+
+SUBROUTINE pxrpush_particles_part1
+USE particles
+USE constants
+USE fields
+USE params
+USE shared_data
+USE tiling
+IMPLICIT NONE
+INTEGER(idp) :: ispecies, ix, iy, iz, count
+INTEGER(idp) :: jmin, jmax, kmin, kmax, lmin, lmax
+TYPE(particle_species), POINTER :: curr
+TYPE(particle_tile), POINTER :: curr_tile
+REAL(num) :: tdeb, tend
+INTEGER(idp) :: nxc, nyc, nzc, ipmin,ipmax, np,ip
+INTEGER(idp) :: nblk=1000000
+
+tdeb=MPI_WTIME()
+!$OMP PARALLEL DO COLLAPSE(3) SCHEDULE(runtime) DEFAULT(NONE) &
+!$OMP SHARED(ntilex,ntiley,ntilez,nspecies,species_parray, &
+!$OMP nxjguards,nyjguards,nzjguards,ex,ey,ez,bx,by,bz,dx,dy,dz,dt,nblk) &
+!$OMP PRIVATE(ix,iy,iz,ispecies,curr,curr_tile,count,jmin,jmax,kmin,kmax,lmin, &
+!$OMP lmax,nxc,nyc,nzc, ipmin,ipmax,ip,np)
+DO iz=1, ntilez ! LOOP ON TILES
+    DO iy=1, ntiley
+        DO ix=1, ntilex
+            DO ispecies=1, nspecies ! LOOP ON SPECIES
+                ! - Get current tile properties
+                ! - Init current tile variables
+                curr=>species_parray(ispecies)
+                curr_tile=>curr%array_of_tiles(ix,iy,iz)
+                count=curr_tile%np_tile
+                jmin=curr_tile%nx_tile_min-nxjguards
+                jmax=curr_tile%nx_tile_max+nxjguards
+                kmin=curr_tile%ny_tile_min-nyjguards
+                kmax=curr_tile%ny_tile_max+nyjguards
+                lmin=curr_tile%nz_tile_min-nzjguards
+                lmax=curr_tile%nz_tile_max+nzjguards
+                nxc=curr_tile%nx_cells_tile
+                nyc=curr_tile%ny_cells_tile
+                nzc=curr_tile%nz_cells_tile
+                curr_tile%part_ex = 0.0_num
+                curr_tile%part_ey = 0.0_num
+                curr_tile%part_ez = 0.0_num
+                curr_tile%part_bx=0.0_num
+                curr_tile%part_by=0.0_num
+                curr_tile%part_bz=0.0_num
+                !!! ---- Loop by blocks over particles in a tile (blocking)
+                DO ip=1,count,nblk
+                    np=MIN(count-ip+1,nblk)
+                    ipmin=ip
+                    ipmax=ip+np-1
+                    !!! --- Gather electric field on particles
+                    CALL gete3d_energy_conserving_1_1_1(np,curr_tile%part_x(ipmin:ipmax),curr_tile%part_y(ipmin:ipmax),&
+                                      curr_tile%part_z(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax),                   &
+                                      curr_tile%part_ey(ipmin:ipmax),curr_tile%part_ez(ipmin:ipmax),                   &
+                                      curr_tile%x_grid_tile_min,curr_tile%y_grid_tile_min,                             &
+                                      curr_tile%z_grid_tile_min, dx,dy,dz,curr_tile%nx_cells_tile,                     &
+                                      curr_tile%ny_cells_tile,curr_tile%nz_cells_tile,nxjguards,nyjguards,             &
+                                      nzjguards,ex(jmin:jmax,kmin:kmax,lmin:lmax),ey(jmin:jmax,kmin:kmax,lmin:lmax),   &
+                                      ez(jmin:jmax,kmin:kmax,lmin:lmax))
+                    !!! --- Gather magnetic fields on particles
+                    CALL getb3d_energy_conserving_1_1_1(np,curr_tile%part_x(ipmin:ipmax),curr_tile%part_y(ipmin:ipmax),&
+                                      curr_tile%part_z(ipmin:ipmax), curr_tile%part_bx(ipmin:ipmax),                      &
+                                      curr_tile%part_by(ipmin:ipmax),curr_tile%part_bz(ipmin:ipmax),                      &
+                                      curr_tile%x_grid_tile_min,curr_tile%y_grid_tile_min,                                &
+                                      curr_tile%z_grid_tile_min, dx,dy,dz,curr_tile%nx_cells_tile,                        &
+                                      curr_tile%ny_cells_tile,curr_tile%nz_cells_tile,nxjguards,nyjguards,                &
+                                      nzjguards,bx(jmin:jmax,kmin:kmax,lmin:lmax),by(jmin:jmax,kmin:kmax,lmin:lmax),      &
+                                      bz(jmin:jmax,kmin:kmax,lmin:lmax))
+                    !!! --- Push velocity with E half step
+                    CALL epush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
+                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_ex(ipmin:ipmax), curr_tile%part_ey(ipmin:ipmax), &
+                    curr_tile%part_ez(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    !!! --- Push velocity with B half step
+                    CALL bpush_v(np,curr_tile%part_ux(ipmin:ipmax), curr_tile%part_uy(ipmin:ipmax),                 &
+                    curr_tile%part_uz(ipmin:ipmax), curr_tile%part_bx(ipmin:ipmax), curr_tile%part_by(ipmin:ipmax), &
+                    curr_tile%part_bz(ipmin:ipmax), curr%charge,curr%mass,dt*0.5_num)
+                    !!! --- Push velocity with B half step
+                END DO
+            END DO! END LOOP ON SPECIES
+        END DO
+    END DO
+END DO! END LOOP ON TILES
+!$OMP END PARALLEL DO
+tend=MPI_WTIME()
+pushtime=pushtime+(tend-tdeb)
+END SUBROUTINE pxrpush_particles_part1
 !===============================================================================
 !  Advance particle positions
 SUBROUTINE pushxyz(np,xp,yp,zp,uxp,uyp,uzp,dt)
