0f9c569de95a32eb5e9f68890a8de766d00d928c
Jean-Luc Vay <jlvay@jlvay2.lbl.gov>
2016-01-15 08:39:56 -0800

    Various changes.

--- example_scripts_python/testwwarp.py
+++ example_scripts_python/testwwarp.py
@@ -4,44 +4,46 @@ import os
 currentdir=os.getcwd()
 sys.path.append(currentdir+'/python_bin/')
 print(currentdir+'/python_bin/')
-import picsarpy as pxr
+import picsarpy as pxrpy
 import numpy as np
 import warp as wp
+
+pxr = pxrpy.picsar
 
 #### Input parameters (replace input file)
 pxr.default_init()
 ##Simulation box size and duration
-pxr.picsar.nx_global_grid=10
-pxr.picsar.ny_global_grid=10
-pxr.picsar.nz_global_grid=10
-pxr.picsar.xmin=0.0
-pxr.picsar.ymin=0.0
-pxr.picsar.zmin=0.0
-pxr.picsar.tmax=0.5
+pxr.nx_global_grid=10
+pxr.ny_global_grid=10
+pxr.nz_global_grid=10
+pxr.xmin=0.0
+pxr.ymin=0.0
+pxr.zmin=0.0
+pxr.tmax=0.5
 
 ## Resolution
-pxr.picsar.dx=6.5e-7
-pxr.picsar.dy=6.5e-7
-pxr.picsar.dz=6.5e-7
+pxr.dx=6.5e-7
+pxr.dy=6.5e-7
+pxr.dz=6.5e-7
 
 ## Maxwell solver
-pxr.picsar.norderx=2
-pxr.picsar.nordery=2
-pxr.picsar.norderz=2
+pxr.norderx=2
+pxr.nordery=2
+pxr.norderz=2
 
 
 ## Current deposition order
-pxr.picsar.nox=1
-pxr.picsar.noy=1
-pxr.picsar.noz=1
+pxr.nox=1
+pxr.noy=1
+pxr.noz=1
 
 ## Tiling parameters
-pxr.picsar.ntilex=5
-pxr.picsar.ntiley=5
-pxr.picsar.ntilez=5
+pxr.ntilex=5
+pxr.ntiley=5
+pxr.ntilez=5
 
 ## Particles species
-pxr.picsar.nspecies=2 # Set number of particle species
+pxr.nspecies=2 # Set number of particle species
 pxr.pdistr=2 # Random distribution for particles
 # Set particle species 1 properties
 pxr.set_particle_species_properties(1,"electron",1.,-1.,10,0.,0.,0.,1.3,1.3,1.3,0.,0.,0.1,0.,0.,0.)
@@ -53,7 +55,7 @@ pxr.set_particle_species_properties(2,"proton",1836.,-1.,10,0.,0.,0.,1.3,1.3,1.3
 #### MPI INIT
 pxr.mpi_minimal_init()
 pxr.mpi_initialise()
-mpirank= pxr.picsar.rank
+mpirank= pxr.rank
 
 
 #### Init particle distributions  for each species (with tiles)
@@ -68,6 +70,7 @@ ntsteps=10
 #endt=MPI.Wtime()
 #if (mpirank==0):
 #    print("Total simulation time with step in Fortran 90 (s) ="+str(endt-start))
+
 def steppy(nt):
     for i in range(0,nt):
         # Push particles
@@ -81,6 +84,7 @@ def steppy(nt):
         # Push EM fields
         pxr.push_bfield()
         pxr.bfield_bcs()
+        
         pxr.push_efield()
         pxr.efield_bcs()
         pxr.push_bfield()
@@ -91,31 +95,31 @@ def steppy(nt):
 
         # print diags
         if (mpirank==0):
-            print("Iteration number "+str(i)+"|| time (s) ="+str(pxr.picsar.dt*i))
+            print("Iteration number "+str(i)+"|| time (s) ="+str(pxr.dt*i))
 
 
 def pntfieldswarp():
-    wp.w3d.nx = pxr.picsar.nx_global
-    wp.w3d.ny = pxr.picsar.ny_global
-    wp.w3d.nz = pxr.picsar.nz_global
-    wp.w3d.xmmin = pxr.picsar.xmin
-    wp.w3d.xmmax = pxr.picsar.xmax
-    wp.w3d.ymmin = pxr.picsar.ymin
-    wp.w3d.ymmax = pxr.picsar.ymax
-    wp.w3d.zmmin = pxr.picsar.zmin
-    wp.w3d.zmmax = pxr.picsar.zmax
+    wp.w3d.nx = pxr.nx_global
+    wp.w3d.ny = pxr.ny_global
+    wp.w3d.nz = pxr.nz_global
+    wp.w3d.xmmin = pxr.xmin
+    wp.w3d.xmmax = pxr.xmax
+    wp.w3d.ymmin = pxr.ymin
+    wp.w3d.ymmax = pxr.ymax
+    wp.w3d.zmmin = pxr.zmin
+    wp.w3d.zmmax = pxr.zmax
     
-    wp.em = wp.EM3D(nxguard=pxr.picsar.nxguards,
-                    nyguard=pxr.picsar.nyguards,
-                    nzguard=pxr.picsar.nzguards)
+    wp.em = wp.EM3D(nxguard=pxr.nxguards,
+                    nyguard=pxr.nyguards,
+                    nzguard=pxr.nzguards)
 
     wp.em.finalize()
-    del wp.em.fields.Ex; wp.em.fields.Ex = pxr.picsar.ex
-    del wp.em.fields.Ey; wp.em.fields.Ey = pxr.picsar.ey
-    del wp.em.fields.Ez; wp.em.fields.Ez = pxr.picsar.ez
-    del wp.em.fields.Bx; wp.em.fields.Ex = pxr.picsar.bx
-    del wp.em.fields.By; wp.em.fields.Ey = pxr.picsar.by
-    del wp.em.fields.Bz; wp.em.fields.Ez = pxr.picsar.bz
+    del wp.em.fields.Ex; wp.em.fields.Ex = pxr.ex
+    del wp.em.fields.Ey; wp.em.fields.Ey = pxr.ey
+    del wp.em.fields.Ez; wp.em.fields.Ez = pxr.ez
+    del wp.em.fields.Bx; wp.em.fields.Ex = pxr.bx
+    del wp.em.fields.By; wp.em.fields.Ey = pxr.by
+    del wp.em.fields.Bz; wp.em.fields.Ez = pxr.bz
     
 pntfieldswarp()
 
@@ -128,7 +132,9 @@ steppy(ntsteps)
 #if (mpirank==0):
 #    print("Total simulation time with step in python (s) ="+str(endt-start))
 
-wp.winon()
+wp.em.pfezg(ncolor=21)
+
+#wp.winon()
 
 
 ## MPICLOSE
