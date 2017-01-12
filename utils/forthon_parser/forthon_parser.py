4a9699e4547094ab0fb38527af8094e838b9f639
mathieu_lobet <mathieu.lobet@gmail.com>
2017-01-12 14:34:26 -0800

    Correction mistake .o in forthon parser

--- utils/forthon_parser/forthon_parser.py
+++ utils/forthon_parser/forthon_parser.py
@@ -1109,11 +1109,11 @@ listfiles=["modules.F90", \
            "particle_pushers/vay_pusher/vay_3d.F90", \
            "particle_pushers/particle_pusher_manager_2d.F90", \
            "particle_pushers/particle_pusher_manager_3d.F90", \
-           "particle_deposition/current_deposition/direct/direct_current_deposition_3d.o", \
-           "particle_deposition/current_deposition/esirkepov/esirkepov_2d.o", \
-           "particle_deposition/current_deposition/esirkepov/esirkepov_3d.o", \
-           "particle_deposition/current_deposition/current_deposition_manager_2d.o", \
-           "particle_deposition/current_deposition/current_deposition_manager_3d.o", \
+           "particle_deposition/current_deposition/direct/direct_current_deposition_3d.F90", \
+           "particle_deposition/current_deposition/esirkepov/esirkepov_2d.F90", \
+           "particle_deposition/current_deposition/esirkepov/esirkepov_3d.F90", \
+           "particle_deposition/current_deposition/current_deposition_manager_2d.F90", \
+           "particle_deposition/current_deposition/current_deposition_manager_3d.F90", \
            "field_gathering_2d.F90", \
            "field_gathering.F90", \
            "field_gathering_3d_o1.F90",\
