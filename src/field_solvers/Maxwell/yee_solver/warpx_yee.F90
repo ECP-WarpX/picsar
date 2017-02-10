e52228c3189fc7567981032fb477eaa23004f01d
Remi Lehe <remi.lehe@normalesup.org>
2017-02-10 09:58:16 -0800

    Implemented push E with matched field shapes

--- src/field_solvers/Maxwell/yee_solver/warpx_yee.F90
+++ src/field_solvers/Maxwell/yee_solver/warpx_yee.F90
@@ -1,19 +1,45 @@
 
 ! ______________________________________________________________________________
 !> @brief
-!> This subroutine pushes the electric field with the 3D Yee FDTD 
+!> This subroutine pushes the electric field with the 3D Yee FDTD
 !> scheme (order 2).
-!> This subroutine is adapted for Boxlib and do not contain $OMP parallel 
+!>
+!> This subroutine is adapted for Boxlib and do not contain $OMP parallel
 !> regions.
-!
+!> Also, this routine allows to specify the indices between which
+!> the fields should be updated. These indices are not necessarily the same
+!> as the full bounds of the field arrays. For instance, if this routine is
+!> called by different OpenMP threads, each thread will handle a different part
+!> of the array, and thus the corresponding indices that are passed as argument
+!> to this routine will be different.
+!>
 !> @author
 !> Weiqun Zhang
 !> Mathieu Lobet
-!
+!> Remi Lehe
+!>
+!> @param[in] xlo the lowest indices (in 3D) at which to update the Ex field
+!> @param[in] xhi the highest indices (in 3D) at which to update the Ex field
+!> @param[in] ylo the lowest indices (in 3D) at which to update the Ey field
+!> @param[in] yhi the highest indices (in 3D) at which to update the Ey field
+!> @param[in] zlo the lowest indices (in 3D) at which to update the Ez field
+!> @param[in] zhi the highest indices (in 3D) at which to update the Ez field
+!> @param[inout] ex the array of values of the Ex field
+!> @param[in] exlo the lowest bound (in 3D) of the array `ex` (`exlo` is always lower than `xlo`)
+!> @param[in] exhi the highest bound (in 3D) of the array `ex` (`exlo` is always higher than `xlo`)
+!> @param[inout] ey the array of values of the Ey field
+!> @param[in] eylo the lowest bound (in 3D) of the array `ey` (`eylo` is always lower than `ylo`)
+!> @param[in] eyhi the highest bound (in 3D) of the array `ey` (`eylo` is always higher than `ylo`)
+!> @param[inout] ez the array of values of the Ez field
+!> @param[in] ezlo the lowest bound (in 3D) of the array `ez` (`ezlo` is always lower than `zlo`)
+!> @param[in] ezhi the highest bound (in 3D) of the array `ez` (`ezlo` is always higher than `zlo`)
+!>
+!>
+!>
 !> @date
 !> Creation 2015
 subroutine warpx_pxr_push_em3d_evec( &
-     lo, hi, &
+     xlo, xhi, ylo, yhi, zlo, zhi, &
      ex,exlo,exhi,&
      ey,eylo, eyhi, &
      ez,ezlo, ezhi, &
@@ -30,39 +56,50 @@ subroutine warpx_pxr_push_em3d_evec( &
 
   use constants
 
-  integer, intent(in) :: lo(3), hi(3), &
+  integer, intent(in) :: &
+       xlo(3), xhi(3), ylo(3), yhi(3), zlo(3), zhi(3), &
        exlo(3),exhi(3),eylo(3),eyhi(3),ezlo(3),ezhi(3),&
        bxlo(3),bxhi(3),bylo(3),byhi(3),bzlo(3),bzhi(3),&
        jxlo(3),jxhi(3),jylo(3),jyhi(3),jzlo(3),jzhi(3),&
        norder
-       
+
   real(num), intent(IN OUT):: ex(exlo(1):exhi(1),exlo(2):exhi(2),exlo(3):exhi(3))
   real(num), intent(IN OUT):: ey(eylo(1):eyhi(1),eylo(2):eyhi(2),eylo(3):eyhi(3))
   real(num), intent(IN OUT):: ez(ezlo(1):ezhi(1),ezlo(2):ezhi(2),ezlo(3):ezhi(3))
-  
+
   real(num), intent(IN):: bx(bxlo(1):bxhi(1),bxlo(2):bxhi(2),bxlo(3):bxhi(3))
   real(num), intent(IN):: by(bylo(1):byhi(1),bylo(2):byhi(2),bylo(3):byhi(3))
   real(num), intent(IN):: bz(bzlo(1):bzhi(1),bzlo(2):bzhi(2),bzlo(3):bzhi(3))
-  
+
   real(num), intent(IN):: jx(jxlo(1):jxhi(1),jxlo(2):jxhi(2),jxlo(3):jxhi(3))
   real(num), intent(IN):: jy(jylo(1):jyhi(1),jylo(2):jyhi(2),jylo(3):jyhi(3))
   real(num), intent(IN):: jz(jzlo(1):jzhi(1),jzlo(2):jzhi(2),jzlo(3):jzhi(3))
-  
+
   real(num), intent(IN) :: mudt,dtsdx,dtsdy,dtsdz
 
   integer :: j,k,l
 
-  do l       = lo(3), hi(3)
-     do k    = lo(2), hi(2)
-        do j = lo(1), hi(1)           
-           Ex(j,k,l) = Ex(j,k,l) + dtsdy * (Bz(j,k,l) - Bz(j,k-1,l  )) &
+  do l         = xlo(3), xhi(3)
+      do k     = xlo(2), xhi(2)
+          do j = xlo(1), xhi(1)
+            Ex(j,k,l) = Ex(j,k,l) + dtsdy * (Bz(j,k,l) - Bz(j,k-1,l  )) &
                                  - dtsdz * (By(j,k,l) - By(j,k  ,l-1)) &
                                  - mudt  * jx(j,k,l)
-              
-           Ey(j,k,l) = Ey(j,k,l) - dtsdx * (Bz(j,k,l) - Bz(j-1,k,l)) &
+          end do
+      end do
+  end do
+  do l         = ylo(3), yhi(3)
+      do k     = ylo(2), yhi(2)
+          do j = ylo(1), yhi(1)
+            Ey(j,k,l) = Ey(j,k,l) - dtsdx * (Bz(j,k,l) - Bz(j-1,k,l)) &
                                  + dtsdz * (Bx(j,k,l) - Bx(j,k,l-1)) &
                                  - mudt  * jy(j,k,l)
-
+           end do
+       end do
+   end do
+   do l         = zlo(3), zhi(3)
+       do k     = zlo(2), zhi(2)
+           do j = zlo(1), zhi(1)
            Ez(j,k,l) = Ez(j,k,l) + dtsdx * (By(j,k,l) - By(j-1,k  ,l)) &
                                  - dtsdy * (Bx(j,k,l) - Bx(j  ,k-1,l)) &
                                  - mudt  * jz(j,k,l)
@@ -76,11 +113,38 @@ end subroutine warpx_pxr_push_em3d_evec
 
 ! ______________________________________________________________________________
 !> @brief
-!> This subroutine pushes the magnetic field with the 3D Yee FDTD 
+!> This subroutine pushes the magnetic field with the 3D Yee FDTD
 !> scheme (order 2).
-!> This subroutine is adapted for Boxlib and do not contain $OMP parallel 
+!>
+!> This subroutine is adapted for Boxlib and do not contain $OMP parallel
 !> regions.
-!
+!> Also, this routine allows to specify the indices between which
+!> the fields should be updated. These indices are not necessarily the same
+!> as the full bounds of the field arrays. For instance, if this routine is
+!> called by different OpenMP threads, each thread will handle a different part
+!> of the array, and thus the corresponding indices that are passed as argument
+!> to this routine will be different.
+!>
+!> @author
+!> Weiqun Zhang
+!> Mathieu Lobet
+!>
+!> @param[in] xlo the lowest indices (in 3D) at which to update the Bx field
+!> @param[in] xhi the highest indices (in 3D) at which to update the Bx field
+!> @param[in] ylo the lowest indices (in 3D) at which to update the By field
+!> @param[in] yhi the highest indices (in 3D) at which to update the By field
+!> @param[in] zlo the lowest indices (in 3D) at which to update the Bz field
+!> @param[in] zhi the highest indices (in 3D) at which to update the Bz field
+!> @param[inout] bx the array of values of the Bx field
+!> @param[in] bxlo the lowest bound (in 3D) of the array `bx` (`bxlo` is always lower than `xlo`)
+!> @param[in] bxhi the highest bound (in 3D) of the array `bx` (`bxlo` is always higher than `xlo`)
+!> @param[inout] by the array of values of the by field
+!> @param[in] bylo the lowest bound (in 3D) of the array `by` (`bylo` is always lower than `ylo`)
+!> @param[in] byhi the highest bound (in 3D) of the array `by` (`bylo` is always higher than `ylo`)
+!> @param[inout] bz the array of values of the bz field
+!> @param[in] bzlo the lowest bound (in 3D) of the array `bz` (`bzlo` is always lower than `zlo`)
+!> @param[in] bzhi the highest bound (in 3D) of the array `bz` (`bzlo` is always higher than `zlo`)
+!>
 !> @author
 !> Weiqun Zhang
 !> Mathieu Lobet
@@ -105,40 +169,40 @@ subroutine warpx_pxr_push_em3d_bvec( &
        exlo(3),exhi(3),eylo(3),eyhi(3),ezlo(3),ezhi(3),&
        bxlo(3),bxhi(3),bylo(3),byhi(3),bzlo(3),bzhi(3),&
        norder
-       
+
   real(num), intent(IN):: ex(exlo(1):exhi(1),exlo(2):exhi(2),exlo(3):exhi(3))
   real(num), intent(IN):: ey(eylo(1):eyhi(1),eylo(2):eyhi(2),eylo(3):eyhi(3))
   real(num), intent(IN):: ez(ezlo(1):ezhi(1),ezlo(2):ezhi(2),ezlo(3):ezhi(3))
-  
+
   real(num), intent(INOUT):: bx(bxlo(1):bxhi(1),bxlo(2):bxhi(2),bxlo(3):bxhi(3))
   real(num), intent(INOUT):: by(bylo(1):byhi(1),bylo(2):byhi(2),bylo(3):byhi(3))
   real(num), intent(INOUT):: bz(bzlo(1):bzhi(1),bzlo(2):bzhi(2),bzlo(3):bzhi(3))
-  
+
   real(num), intent(IN) :: dtsdx,dtsdy,dtsdz
 
   integer :: j,k,l
 
   do l       = xlo(3), xhi(3)
      do k    = xlo(2), xhi(2)
-        do j = xlo(1), xhi(1)           
+        do j = xlo(1), xhi(1)
            Bx(j,k,l) = Bx(j,k,l) - dtsdy * (Ez(j  ,k+1,l  ) - Ez(j,k,l)) &
                                  + dtsdz * (Ey(j  ,k  ,l+1) - Ey(j,k,l))
         end do
      end do
   end do
-  
+
   do l       = ylo(3), yhi(3)
      do k    = ylo(2), yhi(2)
-        do j = ylo(1), yhi(1)           
+        do j = ylo(1), yhi(1)
            By(j,k,l) = By(j,k,l) + dtsdx * (Ez(j+1,k  ,l  ) - Ez(j,k,l)) &
                                  - dtsdz * (Ex(j  ,k  ,l+1) - Ex(j,k,l))
         end do
      end do
   end do
-  
+
   do l       = zlo(3), zhi(3)
      do k    = zlo(2), zhi(2)
-        do j = zlo(1), zhi(1)           
+        do j = zlo(1), zhi(1)
            Bz(j,k,l) = Bz(j,k,l) - dtsdx * (Ey(j+1,k  ,l  ) - Ey(j,k,l)) &
                                  + dtsdy * (Ex(j  ,k+1,l  ) - Ex(j,k,l))
         end do
@@ -150,9 +214,9 @@ end subroutine warpx_pxr_push_em3d_bvec
 
 ! ______________________________________________________________________________
 !> @brief
-!> This subroutine pushes the electric field with the 2D Yee FDTD 
+!> This subroutine pushes the electric field with the 2D Yee FDTD
 !> scheme (order 2).
-!> This subroutine is adapted for Boxlib and do not contain $OMP parallel 
+!> This subroutine is adapted for Boxlib and do not contain $OMP parallel
 !> regions.
 !
 !> @author
@@ -184,19 +248,19 @@ subroutine warpx_pxr_push_em2d_evec( &
        bxlo(2),bxhi(2),bylo(2),byhi(2),bzlo(2),bzhi(2),&
        jxlo(2),jxhi(2),jylo(2),jyhi(2),jzlo(2),jzhi(2),&
        norder
-       
+
   real(num), intent(IN OUT):: ex(exlo(1):exhi(1),exlo(2):exhi(2))
   real(num), intent(IN OUT):: ey(eylo(1):eyhi(1),eylo(2):eyhi(2))
   real(num), intent(IN OUT):: ez(ezlo(1):ezhi(1),ezlo(2):ezhi(2))
-  
+
   real(num), intent(IN):: bx(bxlo(1):bxhi(1),bxlo(2):bxhi(2))
   real(num), intent(IN):: by(bylo(1):byhi(1),bylo(2):byhi(2))
   real(num), intent(IN):: bz(bzlo(1):bzhi(1),bzlo(2):bzhi(2))
-  
+
   real(num), intent(IN):: jx(jxlo(1):jxhi(1),jxlo(2):jxhi(2))
   real(num), intent(IN):: jy(jylo(1):jyhi(1),jylo(2):jyhi(2))
   real(num), intent(IN):: jz(jzlo(1):jzhi(1),jzlo(2):jzhi(2))
-  
+
   real(num), intent(IN) :: mudt,dtsdx,dtsdy,dtsdz
 
   integer :: j,k
@@ -204,10 +268,10 @@ subroutine warpx_pxr_push_em2d_evec( &
   ! dtsdy should be be used.  It is set to nan by WarpX.
 
   do k    = lo(2), hi(2)
-     do j = lo(1), hi(1)           
+     do j = lo(1), hi(1)
         Ex(j,k) = Ex(j,k) - dtsdz * (By(j,k) - By(j,k-1)) &
                           - mudt  * jx(j,k)
-              
+
         Ey(j,k) = Ey(j,k) - dtsdx * (Bz(j,k) - Bz(j-1,k)) &
                           + dtsdz * (Bx(j,k) - Bx(j,k-1)) &
                           - mudt  * jy(j,k)
@@ -223,9 +287,9 @@ end subroutine warpx_pxr_push_em2d_evec
 
 ! ______________________________________________________________________________
 !> @brief
-!> This subroutine pushes the magnetic field with the 2D Yee FDTD 
+!> This subroutine pushes the magnetic field with the 2D Yee FDTD
 !> scheme (order 2).
-!> This subroutine is adapted for Boxlib and do not contain $OMP parallel 
+!> This subroutine is adapted for Boxlib and do not contain $OMP parallel
 !> regions.
 !
 !> @author
@@ -252,15 +316,15 @@ subroutine warpx_pxr_push_em2d_bvec( &
        exlo(2),exhi(2),eylo(2),eyhi(2),ezlo(2),ezhi(2),&
        bxlo(2),bxhi(2),bylo(2),byhi(2),bzlo(2),bzhi(2),&
        norder
-       
+
   real(num), intent(IN):: ex(exlo(1):exhi(1),exlo(2):exhi(2))
   real(num), intent(IN):: ey(eylo(1):eyhi(1),eylo(2):eyhi(2))
   real(num), intent(IN):: ez(ezlo(1):ezhi(1),ezlo(2):ezhi(2))
-  
+
   real(num), intent(INOUT):: bx(bxlo(1):bxhi(1),bxlo(2):bxhi(2))
   real(num), intent(INOUT):: by(bylo(1):byhi(1),bylo(2):byhi(2))
   real(num), intent(INOUT):: bz(bzlo(1):bzhi(1),bzlo(2):bzhi(2))
-  
+
   real(num), intent(IN) :: dtsdx,dtsdy,dtsdz
 
   integer :: j,k
@@ -268,7 +332,7 @@ subroutine warpx_pxr_push_em2d_bvec( &
   ! dtsdy should be be used.  It is set to nan by WarpX.
 
   do k    = lo(2), hi(2)
-     do j = lo(1), hi(1)           
+     do j = lo(1), hi(1)
         Bx(j,k) = Bx(j,k) + dtsdz * (Ey(j  ,k+1) - Ey(j,k))
         By(j,k) = By(j,k) + dtsdx * (Ez(j+1,k  ) - Ez(j,k)) &
                           - dtsdz * (Ex(j  ,k+1) - Ex(j,k))
