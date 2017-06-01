f6bd658fcf24e9edbcf6200f43d4a5e7a3971e26
Henri VINCENTI <henrivincenti@22.196.128.198.in-addr.dhcp.lbnl.us>
2017-06-01 16:38:08 -0700

    Corrected indentation using indentation parser of mpi_derived_types.F90

--- src/parallelization/mpi/mpi_derived_types.F90
+++ src/parallelization/mpi/mpi_derived_types.F90
@@ -42,14 +42,14 @@
 !> Creation 2015
 ! ______________________________________________________________________________
 MODULE mpi_derived_types !#do not parse
-
+  
   USE shared_data
-
+  
   IMPLICIT NONE
-
-CONTAINS
-
-
+  
+  CONTAINS
+  
+  
   ! ____________________________________________________________________________
   !> @brief
   !> create_current_field_derived_type - Creates the derived type
@@ -61,16 +61,16 @@ CONTAINS
   !> @date
   !> Creation 2015
   FUNCTION create_current_grid_derived_type()
-  ! ____________________________________________________________________________
+    ! ____________________________________________________________________________
     INTEGER(isp) :: create_current_grid_derived_type
-
+    
     create_current_grid_derived_type = &
-        create_grid_derived_type(mpidbl, nx, ny, nz, nx_global_grid_min, &
-            ny_global_grid_min, nz_global_grid_min)
-
+    create_grid_derived_type(mpidbl, nx, ny, nz, nx_global_grid_min, &
+    ny_global_grid_min, nz_global_grid_min)
+    
   END FUNCTION create_current_grid_derived_type
-
-
+  
+  
   ! ____________________________________________________________________________
   !> @brief
   !> Create a subarray from the current grid according to the given parameters.
@@ -81,22 +81,22 @@ CONTAINS
   !> @date
   !> Creation 2015
   FUNCTION create_current_grid_subarray(ngx,ngy,ngz)
-  ! ____________________________________________________________________________
-
+    ! ____________________________________________________________________________
+    
     INTEGER(isp) :: create_current_grid_subarray
     INTEGER(idp), INTENT(IN) :: ngx, ngy, ngz
     INTEGER(idp) :: nxloc, nyloc, nzloc
     nxloc=nx+1
     nyloc=ny+1
     nzloc=nz+1
-
-
+    
+    
     create_current_grid_subarray = &
     create_grid_subarray(mpidbl, ngx, ngy, ngz, &
-                              nxloc, nyloc, nzloc)
-
+    nxloc, nyloc, nzloc)
+    
   END FUNCTION create_current_grid_subarray
-
+  
   ! ____________________________________________________________________________
   !> @brief
   !> create_grid_derived_type - Creates a derived type representing the layout
@@ -108,8 +108,8 @@ CONTAINS
   !> @date
   !> Creation 2015
   FUNCTION create_grid_derived_type(mpitype, nx_local, ny_local, nz_local, &
-      cell_start_x_local, cell_start_y_local, cell_start_z_local)
-  ! ____________________________________________________________________________
+    cell_start_x_local, cell_start_y_local, cell_start_z_local)
+    ! ____________________________________________________________________________
     INTEGER(isp), INTENT(IN) :: mpitype
     INTEGER(idp), INTENT(IN) :: nx_local
     INTEGER(idp), INTENT(IN) :: ny_local
@@ -119,22 +119,22 @@ CONTAINS
     INTEGER(idp), INTENT(IN) :: cell_start_z_local
     INTEGER(isp) :: create_grid_derived_type, ndims
     INTEGER(isp), DIMENSION(c_ndims) :: n_local, n_global, start
-
+    
     n_local = (/nx_local, ny_local, nz_local/)
     n_global = (/nx_global, ny_global, nz_global/)
     start = (/cell_start_x_local, cell_start_y_local, cell_start_z_local/)
     ! Old version
     !create_grid_derived_type = &
     !    create_3d_array_derived_type(mpitype, n_local, n_global, start)
-
+    
     ! New version
     ndims=c_ndims
     CALL MPI_TYPE_CREATE_SUBARRAY(ndims,n_global,n_local,start, MPI_ORDER_FORTRAN, &
-                                mpitype,create_grid_derived_type,errcode)
+    mpitype,create_grid_derived_type,errcode)
     CALL MPI_TYPE_COMMIT(create_grid_derived_type ,errcode)
-
+    
   END FUNCTION create_grid_derived_type
-
+  
   ! ____________________________________________________________________________
   !> @brief
   !> create_3d_array_derived_type OLD VERSION - USE MPI_TYPE_CREATE_SUBARRAY
@@ -148,8 +148,8 @@ CONTAINS
   !> @date
   !> Creation 2015
   FUNCTION create_3d_array_derived_type(mpitype, n_local, n_global, start) &
-      RESULT(vec3d_sub)
-  ! ____________________________________________________________________________
+    RESULT(vec3d_sub)
+    ! ____________________________________________________________________________
     INTEGER(isp), INTENT(IN) :: mpitype
     INTEGER(idp), DIMENSION(c_ndims), INTENT(IN) :: n_local
     INTEGER(idp), DIMENSION(c_ndims), INTENT(IN) :: n_global
@@ -158,49 +158,49 @@ CONTAINS
     INTEGER(KIND=MPI_ADDRESS_KIND) :: disp(3), starts(3)
     INTEGER(isp) :: vec2d, vec2d_sub
     INTEGER(isp) :: vec3d, vec3d_sub, typesize
-
+    
     vec2d = MPI_DATATYPE_NULL
     CALL MPI_TYPE_VECTOR(INT(n_local(2),isp), INT(n_local(1),isp), INT(n_global(1),isp), mpitype, &
-        vec2d, errcode)
+    vec2d, errcode)
     CALL MPI_TYPE_COMMIT(vec2d, errcode)
-
+    
     CALL MPI_TYPE_SIZE(mpitype, typesize, errcode)
     starts = start - 1
     lengths = 1
-
+    
     disp(1) = 0
     disp(2) = typesize * (starts(1) + n_global(1) * starts(2))
     disp(3) = typesize * n_global(1) * n_global(2)
     types(1) = MPI_LB
     types(2) = vec2d
     types(3) = MPI_UB
-
+    
     vec2d_sub = MPI_DATATYPE_NULL
     CALL MPI_TYPE_CREATE_STRUCT(c_ndims, lengths, disp, types, vec2d_sub, errcode)
     CALL MPI_TYPE_COMMIT(vec2d_sub, errcode)
-
+    
     vec3d = MPI_DATATYPE_NULL
     CALL MPI_TYPE_CONTIGUOUS(INT(n_local(3),isp), vec2d_sub, vec3d, errcode)
     CALL MPI_TYPE_COMMIT(vec3d, errcode)
-
+    
     disp(1) = 0
     disp(2) = typesize * n_global(1) * n_global(2) * starts(3)
     disp(3) = typesize * n_global(1) * n_global(2) * n_global(3)
     types(1) = MPI_LB
     types(2) = vec3d
     types(3) = MPI_UB
-
+    
     vec3d_sub = MPI_DATATYPE_NULL
     CALL MPI_TYPE_CREATE_STRUCT(c_ndims, lengths, disp, types, vec3d_sub, errcode)
     CALL MPI_TYPE_COMMIT(vec3d_sub, errcode)
-
+    
     CALL MPI_TYPE_FREE(vec2d, errcode)
     CALL MPI_TYPE_FREE(vec2d_sub, errcode)
     CALL MPI_TYPE_FREE(vec3d, errcode)
-
+    
   END FUNCTION create_3d_array_derived_type
-
-
+  
+  
   ! ____________________________________________________________________________
   !> @brief
   !> This subroutine creates a subarray from a given grid according
@@ -221,13 +221,13 @@ CONTAINS
   !> @param[in] n3 number of cells in z
   !
   FUNCTION create_grid_subarray(mpitype, ng1, ng2, ng3, n1, n2, n3)
-  ! ____________________________________________________________________________
-
+    ! ____________________________________________________________________________
+    
     INTEGER(isp), INTENT(IN) :: mpitype
     INTEGER(idp),INTENT(IN) :: ng1,ng2,ng3, n1, n2, n3
     INTEGER(idp), DIMENSION(3) :: n_local, ng, n_global, start
     INTEGER(isp) :: i, ndim, create_grid_subarray
-
+    
     n_local(1) = n1
     n_local(2) = n2
     n_local(3) = n3
@@ -240,17 +240,17 @@ CONTAINS
       n_global(i) = n_local(i) + 2 * ng(i)
     ENDDO
     n_local=n_local-1 ! remove last point
-
+    
     ! old version
     !create_grid_subarray = &
     !      create_3d_array_derived_type(mpitype, n_local, n_global, start)
-
+    
     ! new version
     CALL MPI_TYPE_CREATE_SUBARRAY(ndim,INT(n_global,isp),INT(n_local,isp), &
-                                  INT(start-1,isp), MPI_ORDER_FORTRAN, &
-                                  mpitype,create_grid_subarray,errcode)
+    INT(start-1,isp), MPI_ORDER_FORTRAN, &
+    mpitype,create_grid_subarray,errcode)
     CALL MPI_TYPE_COMMIT(create_grid_subarray,errcode)
-
+    
   END FUNCTION create_grid_subarray
-
+  
 END MODULE mpi_derived_types
