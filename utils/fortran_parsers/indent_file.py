4dd3ee20068746bf36385f4e7c2311d4dbb935fa
Henri VINCENTI <henrivincenti@22.196.128.198.in-addr.dhcp.lbnl.us>
2017-06-01 17:28:11 -0700

    Added doc in parser and file CONTRIBUTING.md

--- utils/fortran_parsers/indent_file.py
+++ utils/fortran_parsers/indent_file.py
@@ -1,4 +1,46 @@
+"""
+ _______________________________________________________________________________
+
+  *** Copyright Notice ***
+
+ "Particle In Cell Scalable Application Resource (PICSAR) v2", Copyright (c)
+ 2016, The Regents of the University of California, through Lawrence Berkeley
+ National Laboratory (subject to receipt of any required approvals from the
+ U.S. Dept. of Energy). All rights reserved.
+
+ If you have questions about your rights to use or distribute this software,
+ please contact Berkeley Lab's Innovation & Partnerships Office at IPO@lbl.gov.
+
+ NOTICE.
+ This Software was developed under funding from the U.S. Department of Energy
+ and the U.S. Government consequently retains certain rights. As such, the U.S.
+ Government has been granted for itself and others acting on its behalf a
+ paid-up, nonexclusive, irrevocable, worldwide license in the Software to
+ reproduce, distribute copies to the public, prepare derivative works, and
+ perform publicly and display publicly, and to permit other to do so.
+
+ PARSER FOR CORRECTING INDENTATION IN FORTRAN FILES 
+ H. VINCENTI - June, 2017
+
+
+ The function correct_indentation below reads Fortran 90 files and correct its indentation 
+ according to guidelines detailed in CONTRIBUTING.md 
+
+ Input arguments: filename
+
+ Outputs:
+ 1. New fortran file filename_forthon.F90 guidelines compliant
+
+ Developers:
+ Henri Vincenti
+
+ Date:
+ Creation: June 1 2017
+ _______________________________________________________________________________
+"""
+
 import re
+
 
 def correct_indentation(input_file,output_file,indent_block):
 	# ----- Parse input file 
