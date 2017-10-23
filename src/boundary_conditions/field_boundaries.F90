6e131f8d491c0cfcd31edd6a893b14553ba02632
haithem kallala <haithem.kallala@cea.fr>
2017-10-23 02:41:51 +0200

    commit

--- src/boundary_conditions/field_boundaries.F90
+++ src/boundary_conditions/field_boundaries.F90
@@ -7,7 +7,8 @@
 ! National Laboratory (subject to receipt of any required approvals from the
 ! U.S. Dept. of Energy). All rights reserved.
 !
-! If you have questions about your rights to use or distribute this software, ! please contact Berkeley Lab's Innovation & Partnerships Office at IPO@lbl.gov.
+! If you have questions about your rights to use or distribute this software,
+! please contact Berkeley Lab's Innovation & Partnerships Office at IPO@lbl.gov.
 !
 ! NOTICE.
 ! This Software was developed under funding from the U.S. Department of Energy
