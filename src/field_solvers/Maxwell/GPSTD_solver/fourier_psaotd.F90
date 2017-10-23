d8e0aca3738e4dcde16e8ab2e616fe14900f0df0
haithem kallala <haithem.kallala@cea.fr>
2017-10-23 10:48:00 +0200

    fixxing a comment

--- src/field_solvers/Maxwell/GPSTD_solver/fourier_psaotd.F90
+++ src/field_solvers/Maxwell/GPSTD_solver/fourier_psaotd.F90
@@ -2,16 +2,19 @@
 !
 ! *** Copyright Notice ***
 !
-! “Particle In Cell Scalable Application Resource (PICSAR) v2”, Copyright (c) 2016, ! The Regents of the University of California, through Lawrence Berkeley National
+! “Particle In Cell Scalable Application Resource (PICSAR) v2”, Copyright (c) 2016,
+! The Regents of the University of California, through Lawrence Berkeley National
 ! Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).
 ! All rights reserved.
 !
-! If you have questions about your rights to use or distribute this software, ! please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+! If you have questions about your rights to use or distribute this software,
+! please contact Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
 !
 ! NOTICE.
 ! This Software was developed under funding from the U.S. Department of Energy
 ! and the U.S. Government consequently retains certain rights. As such, the U.S.
-! Government has been granted for itself and others acting on its behalf a paid-up, ! nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute
+! Government has been granted for itself and others acting on its behalf a paid-up,
+! nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute
 ! copies to the public, prepare derivative works, and perform publicly and display
 ! publicly, and to permit other to do so.
 !
@@ -20,7 +23,11 @@
 !
 ! Purpose:
 ! This file contains subroutines for:
-! (i)  Fourier init (allocation and init of Fourier k-vectors, ! creation of planes for FFTW etc.), ! (ii) Forward/Backward Fourier transform of EM fields using, ! FFTW (Distributed/Shared version), ! (iii) Maxwell push in the spectral space using the Pseudo-Spectral Arbitrary Order
+! (i)  Fourier init (allocation and init of Fourier k-vectors,
+! creation of planes for FFTW etc.),
+! (ii) Forward/Backward Fourier transform of EM fields using,
+! FFTW (Distributed/Shared version),
+! (iii) Maxwell push in the spectral space using the Pseudo-Spectral Arbitrary Order
 ! Analytical Time Domain (PSAOTD) Maxwell solver.
 !
 ! Authors:
