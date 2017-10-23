183e7bc23e8ca868c538bb7e3481ef3d1a2fb352
haithem kallala <haithem.kallala@cea.fr>
2017-10-23 02:48:19 +0200

    commit

--- src/initialization/control_file.F90
+++ src/initialization/control_file.F90
@@ -13,7 +13,8 @@
 ! NOTICE.
 ! This Software was developed under funding from the U.S. Department of Energy
 ! and the U.S. Government consequently retains certain rights. As such, the U.S.
-! Government has been granted for itself and others acting on its behalf a paid-up, ! nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute
+! Government has been granted for itself and others acting on its behalf a paid-up,
+! nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute
 ! copies to the public, prepare derivative works, and perform publicly and display
 ! publicly, and to permit other to do so.
 !
