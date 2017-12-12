c684e0758a119c78e32119b0396e34cc2b37b0b1
haithem kallala <haithem.kallala@cea.fr>
2017-12-12 15:19:28 +0100

    fixing conflict with smilei

--- src/init_external.F90
+++ src/init_external.F90
@@ -46,6 +46,7 @@ MODULE link_external_tools
     fftw_with_mpi = .FALSE. 
     fftw_hybrid = .FALSE.
     fftw_mpi_transpose = .FALSE.
+    l_staggered = .TRUE.
     fftw_threads_ok = .TRUE.
     is_lb_grp = .FALSE.
     c_dim = INT(cdim,idp)
