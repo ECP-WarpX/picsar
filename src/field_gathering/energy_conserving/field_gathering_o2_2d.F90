ee3c68593c07e537b8d5120ffa9d100b858575f5
Remi Lehe <remi.lehe@normalesup.org>
2018-11-12 17:06:53 -0800

    Start implementing scalar field gather

--- src/field_gathering/energy_conserving/field_gathering_o2_2d.F90
+++ src/field_gathering/energy_conserving/field_gathering_o2_2d.F90
@@ -16,19 +16,489 @@
 ! Government has been granted for itself and others acting on its behalf a
 ! paid-up, nonexclusive, irrevocable, worldwide license in the Software to
 ! reproduce, distribute copies to the public, prepare derivative works, and
-! perform publicly and display publicly, and to permit other to do so.
-!
-! FIELD_GATHERING_O2_3D.F90
+! perform publicly and didplay publicly, and to permit other to do so.
 !
 ! Field gathering subroutines in 2D at order 2.
 !
 ! List of subroutines:
 !
+! - gete2dxz_energy_conserving_scalar_2_2
+! - getb2dxz_energy_conserving_scalar_2_2
 ! - pxr_gete2dxz_energy_conserving_vect_2_2
 ! - pxr_getb2dxz_energy_conserving_vect_2_2
 !
 ! ________________________________________________________________________________________
 
+
+! ________________________________________________________________________________________
+!> @brief
+!> Scalar version: gathering of electric field from Yee grid ("energy conserving")
+!> on particles at order 2.
+!
+!> @details
+!> This subroutine is NOT vectorized.
+!
+!> @author
+!> Mathieu Lobet
+!
+!> @date
+!> Creation 2016
+!> Revison 12/04/2016
+!
+!> @param[in] np number of particles
+!> @param[in] xp, zp particle position
+!> @param[inout] ex, ey, ez particle electric field
+!> @param[in] xmin, zmin tile minimum grid position
+!> @param[in] dx, dz space step
+!> @param[in] dt time step
+!> @param[in] exg, eyg, ezg electric field grids
+!> @param[in] exg_nguard, eyg_nguard, ezg_nguard number of guard cells of the exg, eyg, ezg arrays in each direction (1d arrays containing 2 integers)
+!> @param[in] exg_nvalid, eyg_nvalid, ezg_nvalid number of valid gridpoints (i.e. not guard cells) of the exg, eyg, ezg arrays (1d arrays containing 2 integers)
+!> @param[in] l_lower_order_in_v decrease the interpolation order if True
+!
+! ________________________________________________________________________________________
+SUBROUTINE gete2dxz_energy_conserving_scalar_2_2(np, xp, zp, ex, ey, ez, xmin,    &
+  ymin, zmin, dx, dz, exg, exg_nguard, exg_nvalid, eyg, eyg_nguard, eyg_nvalid,     &
+  ezg, ezg_nguard, ezg_nvalid, l_lower_order_in_v)     !#do not wrap
+  USE picsar_precision, ONLY: idp, num, lp
+  IMPLICIT NONE
+  INTEGER(idp)                         :: np
+  INTEGER(idp), intent(in)             :: exg_nguard(3), exg_nvalid(3),               &
+  eyg_nguard(3), eyg_nvalid(3), ezg_nguard(3), ezg_nvalid(3)
+  REAL(num), DIMENSION(np)             :: xp, zp, ex, ey, ez
+  LOGICAL(lp)                              :: l_lower_order_in_v
+  REAL(num), intent(IN):: exg(-exg_nguard(1):exg_nvalid(1)+exg_nguard(1)-1,           &
+  -exg_nguard(2):exg_nvalid(2)+exg_nguard(2)-1,                                       &
+  -exg_nguard(3):exg_nvalid(3)+exg_nguard(3)-1)
+  REAL(num), intent(IN):: eyg(-eyg_nguard(1):eyg_nvalid(1)+eyg_nguard(1)-1,           &
+  -eyg_nguard(2):eyg_nvalid(2)+eyg_nguard(2)-1,                                       &
+  -eyg_nguard(3):eyg_nvalid(3)+eyg_nguard(3)-1)
+  REAL(num), intent(IN):: ezg(-ezg_nguard(1):ezg_nvalid(1)+ezg_nguard(1)-1,           &
+  -ezg_nguard(2):ezg_nvalid(2)+ezg_nguard(2)-1,                                       &
+  -ezg_nguard(3):ezg_nvalid(3)+ezg_nguard(3)-1)
+  REAL(num)                            :: xmin, zmin, dx, dz
+  INTEGER(idp)                         :: ip, j, l
+  INTEGER(idp)                         :: ixmin, ixmax, izmin, izmax
+  INTEGER(idp)                         :: ixmin0, ixmax0, izmin0, izmax0
+  INTEGER(idp)                         :: jj, ll, j0, l0
+  REAL(num)                            :: dxi, dzi, x, z
+  REAL(num)                            :: xint, zint
+  REAL(num)                            :: xintsq, zintsq
+  REAL(num), DIMENSION(-1:1)           :: sx, sx0
+  REAL(num), DIMENSION(-1:1)           :: sz, sz0
+  REAL(num), PARAMETER                 :: onesixth=1.0_num/6.0_num
+  REAL(num), PARAMETER                 :: twothird=2.0_num/3.0_num
+
+  dxi = 1.0_num/dx
+  dzi = 1.0_num/dz
+
+  sx0 = 0
+  sz0 = 0
+  sx = 0
+  sz = 0
+
+  ixmin = -1
+  ixmax = 0
+  izmin = -1
+  izmax = 0
+
+  IF (l_lower_order_in_v) THEN
+
+    ixmin0 = 0
+    ixmax0 = 1
+    izmin0 = 0
+    izmax0 = 1
+
+    !$acc parallel deviceptr(exg, eyg, ezg, xp, zp, ex, ey, ez)
+    !$acc loop gang vector private(sx(-1:1), sz(-1:1), sx0(-1:1), sy0(-1:1), sz0(-1:1))
+    DO ip=1, np
+
+      x = (xp(ip)-xmin)*dxi
+      z = (zp(ip)-zmin)*dzi
+
+      j=nint(x)
+      j0=floor(x-0.5_num)
+
+
+      l=nint(z)
+      l0=floor(z-0.5_num)
+
+      xint=x-j
+      zint=z-l
+
+      xintsq = xint*xint
+      sx(-1) = 0.5_num*(0.5_num-xint)**2
+      sx( 0) = 0.75_num-xintsq
+      sx( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz(-1) = 0.5_num*(0.5_num-zint)**2
+      sz( 0) = 0.75_num-zintsq
+      sz( 1) = 0.5_num*(0.5_num+zint)**2
+
+      xint=x-0.5_num-j0
+      zint=z-0.5_num-l0
+
+      sx0( 0) = 1.0_num-xint
+      sx0( 1) = xint
+
+      sz0( 0) = 1.0_num-zint
+      sz0( 1) = zint
+
+      !$acc loop seq independent collapse(3)
+      DO ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          DO jj = ixmin0, ixmax0
+            ex(ip) = ex(ip) + sx0(jj)*sy(kk)*sz(ll)*exg(j0+jj, k+kk, l+ll)
+          END DO
+      END DO
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      DO ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          DO jj = ixmin, ixmax+1
+            ey(ip) = ey(ip) + sx(jj)*sy0(kk)*sz(ll)*eyg(j+jj, k0+kk, l+ll)
+          END DO
+      END DO
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      DO ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          DO jj = ixmin, ixmax+1
+            ez(ip) = ez(ip) + sx(jj)*sy(kk)*sz0(ll)*ezg(j+jj, k+kk, l0+ll)
+          END DO
+      END DO
+      !$acc end loop
+
+    END DO
+    !$acc end loop
+    !$acc end parallel
+
+    ! __ l_lower_order_in_v false  _____________________________
+  ELSE
+
+    ixmin0 = -1
+    ixmax0 = 1
+    izmin0 = -1
+    izmax0 = 1
+
+    !$acc parallel deviceptr(exg, eyg, ezg, xp, zp, ex, ey, ez)
+    !$acc loop gang vector private(sx(-1:1), sz(-1:1), sx0(-1:1), sy0(-1:1), sz0(-1:1))
+    DO ip=1, np
+
+      x = (xp(ip)-xmin)*dxi
+      z = (zp(ip)-zmin)*dzi
+
+      j=nint(x)
+      j0=floor(x)
+
+
+      l=nint(z)
+      l0=floor(z)
+
+      xint=x-j
+      zint=z-l
+
+      xintsq = xint*xint
+      sx(-1) = 0.5_num*(0.5_num-xint)**2
+      sx( 0) = 0.75_num-xintsq
+      sx( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz(-1) = 0.5_num*(0.5_num-zint)**2
+      sz( 0) = 0.75_num-zintsq
+      sz( 1) = 0.5_num*(0.5_num+zint)**2
+
+      xint=x-0.5_num-j0
+      zint=z-0.5_num-l0
+
+      xintsq = xint*xint
+      sx0(-1) = 0.5_num*(0.5_num-xint)**2
+      sx0( 0) = 0.75_num-xintsq
+      sx0( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz0(-1) = 0.5_num*(0.5_num-zint)**2
+      sz0( 0) = 0.75_num-zintsq
+      sz0( 1) = 0.5_num*(0.5_num+zint)**2
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin0, ixmax0
+            ex(ip) = ex(ip) + sx0(jj)*sy(kk)*sz(ll)*exg(j0+jj, k+kk, l+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin, ixmax+1
+            ey(ip) = ey(ip) + sx(jj)*sy0(kk)*sz(ll)*eyg(j+jj, k0+kk, l+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin, ixmax+1
+            ez(ip) = ez(ip) + sx(jj)*sy(kk)*sz0(ll)*ezg(j+jj, k+kk, l0+ll)
+          end do
+      end do
+      !$acc end loop
+
+    END DO
+    !$acc end loop
+    !$acc end parallel
+  ENDIF
+
+  RETURN
+END SUBROUTINE
+
+! ________________________________________________________________________________________
+!> @brief
+!> Scalar version: Gathering of Magnetic field from Yee grid
+!> ("energy conserving") on particles
+!> at order 2
+!
+!> @details
+!> This function is vectorized
+!
+!> @author
+!> Mathieu Lobet
+!
+!> @date
+!> Creation 2016
+!> Revison 12/05/2016
+!
+!> @param[in] np number of particles
+!> @param[in] xp, zp particle position arrays
+!> @param[inout] bx, by, bz particle magnetic field arrays
+!> @param[in] xmin, zmin tile minimum grid position
+!> @param[in] dx, dz space steps in every directions
+!> @param[in] dt time step
+!> @param[in] bxg, byg, bzg magnetic field grids
+!> @param[in] bxg_nguard, byg_nguard, bzg_nguard number of guard cells of the
+!> bxg, byg, bzg arrays in each direction (1d arrays containing 2 integers)
+!> @param[in] bxg_nvalid, byg_nvalid, bzg_nvalid number of valid gridpoints
+!> (i.e. not guard cells) of the bxg, byg, bzg arrays (1d arrays containing 2 integers)
+!> @param[in] l_lower_order_in_v lower order for the interpolation
+!
+! ________________________________________________________________________________________
+SUBROUTINE getb2dxz_energy_conserving_scalar_2_2(np, xp, zp, bx, by, bz, xmin,    &
+  ymin, zmin, dx, dz, bxg, bxg_nguard, bxg_nvalid, byg, byg_nguard, byg_nvalid,     &
+  bzg, bzg_nguard, bzg_nvalid, l_lower_order_in_v)     !#do not wrap
+  USE picsar_precision, ONLY: idp, num, lp
+  IMPLICIT NONE
+  INTEGER(idp)                         :: np
+  INTEGER(idp), intent(in)                :: bxg_nguard(3), bxg_nvalid(3),            &
+  byg_nguard(3), byg_nvalid(3), bzg_nguard(3), bzg_nvalid(3)
+  REAL(num), DIMENSION(np)             :: xp, zp, bx, by, bz
+  LOGICAL(lp)                              :: l_lower_order_in_v
+  REAL(num), intent(IN):: bxg(-bxg_nguard(1):bxg_nvalid(1)+bxg_nguard(1)-1,           &
+  -bxg_nguard(2):bxg_nvalid(2)+bxg_nguard(2)-1,                                       &
+  -bxg_nguard(3):bxg_nvalid(3)+bxg_nguard(3)-1)
+  REAL(num), intent(IN):: byg(-byg_nguard(1):byg_nvalid(1)+byg_nguard(1)-1,           &
+  -byg_nguard(2):byg_nvalid(2)+byg_nguard(2)-1,                                       &
+  -byg_nguard(3):byg_nvalid(3)+byg_nguard(3)-1)
+  REAL(num), intent(IN):: bzg(-bzg_nguard(1):bzg_nvalid(1)+bzg_nguard(1)-1,           &
+  -bzg_nguard(2):bzg_nvalid(2)+bzg_nguard(2)-1,                                       &
+  -bzg_nguard(3):bzg_nvalid(3)+bzg_nguard(3)-1)
+  REAL(num)                            :: xmin, zmin, dx, dz
+  INTEGER(idp)                         :: ip, j, l, ixmin, ixmax,    &
+  izmin, izmax, ixmin0, ixmax0, izmin0, izmax0, jj, ll, j0, l0
+  REAL(num)                            :: dxi, dzi, x, z, xint, zint
+  REAL(num)                            :: xintsq, zintsq
+  REAL(num), DIMENSION(-1:1)           :: sx, sx0
+  REAL(num), DIMENSION(-1:1)           :: sz, sz0
+  REAL(num), PARAMETER                 :: onesixth=1.0_num/6.0_num,                   &
+  twothird=2.0_num/3.0_num
+
+  dxi = 1.0_num/dx
+  dzi = 1.0_num/dz
+
+  sx=0.0_num
+  sy=0.0_num
+  sz=0.0_num
+  sx0=0.0_num
+  sy0=0.0_num
+  sz0=0.0_num
+
+  ixmin = -1
+  ixmax = 0
+  izmin = -1
+  izmax = 0
+
+  IF (l_lower_order_in_v) THEN
+
+    ixmin0 = 0
+    ixmax0 = 1
+    izmin0 = 0
+    izmax0 = 1
+
+    !$acc parallel deviceptr(bxg, byg, bzg, xp, zp, bx, by, bz)
+    !$acc loop gang vector private(sx(-1:1), sz(-1:1), sx0(-1:1), sy0(-1:1), sz0(-1:1))
+    DO ip=1, np
+
+      x = (xp(ip)-xmin)*dxi
+      z = (zp(ip)-zmin)*dzi
+
+      j=nint(x)
+      j0=floor(x-0.5_num)
+
+
+      l=nint(z)
+      l0=floor(z-0.5_num)
+
+      xint=x-j
+      zint=z-l
+
+      xintsq = xint*xint
+      sx(-1) = 0.5_num*(0.5_num-xint)**2
+      sx( 0) = 0.75_num-xintsq
+      sx( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz(-1) = 0.5_num*(0.5_num-zint)**2
+      sz( 0) = 0.75_num-zintsq
+      sz( 1) = 0.5_num*(0.5_num+zint)**2
+
+      xint=x-0.5_num-j0
+      zint=z-0.5_num-l0
+
+      sx0( 0) = 1.0_num-xint
+      sx0( 1) = xint
+
+      sz0( 0) = 1.0_num-zint
+      sz0( 1) = zint
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin, ixmax+1
+            bx(ip) = bx(ip) + sx(jj)*sy0(kk)*sz0(ll)*bxg(j+jj, k0+kk, l0+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin0, ixmax0
+            by(ip) = by(ip) + sx0(jj)*sy(kk)*sz0(ll)*byg(j0+jj, k+kk, l0+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin0, ixmax0
+            bz(ip) = bz(ip) + sx0(jj)*sy0(kk)*sz(ll)*bzg(j0+jj, k0+kk, l+ll)
+          end do
+      end do
+      !$acc end loop
+
+    END DO
+    !$acc end loop
+    !$acc end parallel
+
+  ELSE
+
+    ixmin0 = -1
+    ixmax0 = 1
+    izmin0 = -1
+    izmax0 = 1
+
+    !$acc parallel deviceptr(bxg, byg, bzg, xp, zp, bx, by, bz)
+    !$acc loop gang vector private(sx(-1:1), sz(-1:1), sx0(-1:1), sy0(-1:1), sz0(-1:1))
+    DO ip=1, np
+
+      x = (xp(ip)-xmin)*dxi
+      z = (zp(ip)-zmin)*dzi
+
+      j=nint(x)
+      j0=floor(x)
+
+
+      l=nint(z)
+      l0=floor(z)
+
+      xint=x-j
+      zint=z-l
+
+      xintsq = xint*xint
+      sx(-1) = 0.5_num*(0.5_num-xint)**2
+      sx( 0) = 0.75_num-xintsq
+      sx( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz(-1) = 0.5_num*(0.5_num-zint)**2
+      sz( 0) = 0.75_num-zintsq
+      sz( 1) = 0.5_num*(0.5_num+zint)**2
+
+      xint=x-0.5_num-j0
+      zint=z-0.5_num-l0
+
+      xintsq = xint*xint
+      sx0(-1) = 0.5_num*(0.5_num-xint)**2
+      sx0( 0) = 0.75_num-xintsq
+      sx0( 1) = 0.5_num*(0.5_num+xint)**2
+
+      zintsq = zint*zint
+      sz0(-1) = 0.5_num*(0.5_num-zint)**2
+      sz0( 0) = 0.75_num-zintsq
+      sz0( 1) = 0.5_num*(0.5_num+zint)**2
+
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin, ixmax+1
+            bx(ip) = bx(ip) + sx(jj)*sy0(kk)*sz0(ll)*bxg(j+jj, k0+kk, l0+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin0, izmax0
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin0, ixmax0
+            by(ip) = by(ip) + sx0(jj)*sy(kk)*sz0(ll)*byg(j0+jj, k+kk, l0+ll)
+          end do
+      end do
+      !$acc end loop
+
+      !$acc loop seq independent collapse(3)
+      do ll = izmin, izmax+1
+          ! Prevent wrong vectorization from the compiler
+          !DIR$ NOVECTOR
+          do jj = ixmin0, ixmax0
+            bz(ip) = bz(ip) + sx0(jj)*sy0(kk)*sz(ll)*bzg(j0+jj, k0+kk, l+ll)
+          end do
+      end do
+      !$acc end loop
+    END DO
+    !$acc end loop
+    !$acc end parallel
+  ENDIF
+  RETURN
+END SUBROUTINE
 
 ! ________________________________________________________________________________________
 !> @brief
@@ -60,7 +530,7 @@
 subroutine pxr_gete2dxz_energy_conserving_vect_2_2( np, xp, zp, ex, ey, ez, xmin,     &
   zmin, dx, dz, exg, exg_nguard, exg_nvalid, eyg, eyg_nguard, eyg_nvalid, ezg,          &
   ezg_nguard, ezg_nvalid, lvect, l_lower_order_in_v)     !#do not wrap
-  USE picsar_precision, ONLY: idp, num, isp
+  USE picsar_precision, ONLY: idp, num
   implicit none
   integer(idp)                  :: np
   integer(idp), intent(IN)      :: exg_nguard(2), exg_nvalid(2), eyg_nguard(2),       &
@@ -75,8 +545,8 @@ subroutine pxr_gete2dxz_energy_conserving_vect_2_2( np, xp, zp, ex, ey, ez, xmin
   REAL(num), intent(IN):: ezg(-ezg_nguard(1):ezg_nvalid(1)+ezg_nguard(1)-1, 1,        &
   -ezg_nguard(2):ezg_nvalid(2)+ezg_nguard(2)-1)
   real(num)                     :: xmin, zmin, dx, dz
-  integer(isp)                  :: ip, j, l, j0, l0
-  integer(isp)                  :: nn, n
+  integer(idp)                  :: ip, j, l, j0, l0
+  integer(idp)                  :: nn, n
   real(num)                     :: dxi, dzi, x, z, xint, zint
   real(num)                     :: xintsq, zintsq
   real(num), DIMENSION(lvect, -1:1)    :: sx, sx0
