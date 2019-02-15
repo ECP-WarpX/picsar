70b19bb60f479ebff9bdc6d4fec2b760356ee12e
Luca Fedeli <luca.fedeli@cea.fr>
2019-02-15 16:07:09 +0100

    added compile time checks on numerical types sizes

--- QED_module_test/multi_physics/commons.h
+++ QED_module_test/multi_physics/commons.h
@@ -1,15 +1,12 @@
 #ifndef __PMP_COMMONS__
 #define __PMP_COMMONS__
 
-//#include <vector>
-//#include <array>
-//#include <functional>
+#include<cstdint>
+
+//Common definitions for the multi_physics library (header only)
 
 namespace picsar{
     namespace multi_physics{
-
-        //typedef std::function<void(momenta_list&, const em_field_list&, double mass, double charge, ttime)> mom_pusher_function;
-
         const double _km = 1.0e3;
         const double _m = 1.0;
         const double _cm = 1.0e-2;
@@ -18,6 +15,10 @@ namespace picsar{
         const double _nm = 1.0e-9;
 
         const double classical_electron_radius = 2.8179403267e-13 * _cm;
+
+        //some checks on the size of the fundamental types
+        static_assert(sizeof(int) == 4, "int type is not 32 bit!");
+        static_assert(sizeof(double) == 8, "double type is not 64 bit!");
     }
 }
 
