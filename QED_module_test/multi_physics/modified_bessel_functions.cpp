579680abb06f0fd4e41d133fc5a275455e78e050
Luca Fedeli <luca.fedeli@cea.fr>
2019-02-18 10:46:56 +0100

    Added special functions from boost if C++17 is not available

--- QED_module_test/multi_physics/modified_bessel_functions.cpp
+++ QED_module_test/multi_physics/modified_bessel_functions.cpp
@@ -2,8 +2,8 @@
 
 using namespace picsar::multi_physics;
 
-//C++17 has built-in support for modified bessel functions
-#if __cplusplus > 201402L
+//C++17 has built-in support for modified bessel functions. Use this as first option
+#ifdef BESSEL_BUILD_WITH_STL
 double k_1_3(double){
     return std::cyl_bessel_k(1.0/3.0, x);
 }
@@ -11,14 +11,32 @@ double k_1_3(double){
 double k_2_3(double){
     return std::cyl_bessel_k(2.0/3.0, x);
 }
-#else
-double k_1_3(double){
-    // TO IMPLEMENT
+
+//If not, link to boost libray
+#elif defined(BESSEL_BUILD_WITH_BOOST)
+double k_1_3(double x){
+     boost::math::cyl_bessel_k(1.0/3.0, x);
     return 0.0;
 }
 
-double k_2_3(double){
-    // TO IMPLEMENT
+double k_2_3(double x){
+     boost::math::cyl_bessel_k(2.0/3.0, x);
     return 0.0;
 }
+
+//If everything fails, use directly Boost source files (TO DO)
+#else
+
+double k_1_3(double x){
+    // TO IMPLEMENT (fails for safety reasons)
+    throw std::logic_error("Function not yet implemented");
+    return 0.0;
+}
+
+double k_2_3(double x){
+    // TO IMPLEMENT (fails for safety reasons)
+    throw std::logic_error("Function not yet implemented");
+    return 0.0;
+}
+
 #endif
--- QED_module_test/multi_physics/modified_bessel_functions.h
+++ QED_module_test/multi_physics/modified_bessel_functions.h
@@ -3,6 +3,26 @@
 
 #include <cmath>
 
+//Set build option for the Bessel functions.
+// 1) from STL (if C++17)
+// 2) from Boost library (if available)
+// 3) using built-in code
+
+#if __cplusplus > 201402L
+    #define BESSEL_BUILD_WITH_STL
+#else
+    #if HAS_BOOST_MATH
+        #define BESSEL_BUILD_WITH_BOOST
+    #else
+        #define BESSEL_BUILD_WITH_BUILTIN
+    #endif
+#endif
+
+
+#ifdef BESSEL_BUILD_WITH_BOOST
+    #include <boost/math/special_functions/bessel.hpp>
+#endif
+
 namespace picsar{
     namespace multi_physics{
         double k_1_3(double);
