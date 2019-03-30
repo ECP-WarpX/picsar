83f35cbea1184cc5491b02f9df1e94802a196071
Remi Lehe <remi.lehe@normalesup.org>
2019-03-30 07:48:06 -0700

    Restrict number of mpi ranks in automated tests

--- Acceptance_testing/Fortran_tests/test_plasma_drift/test_plasma_drift.py
+++ Acceptance_testing/Fortran_tests/test_plasma_drift/test_plasma_drift.py
@@ -113,7 +113,7 @@ def test_plasma_drift(tpath,trun,ttest,tshow):
     os.putenv('OMP_NUM_THREADS','2')
     call(["rm","RESULTS/*"])
     call(["mkdir","-p","RESULTS"])
-    call(["mpirun","-n","4","./picsar"])
+    call(["mpirun","-n","2","./picsar"])
     #call(["sh","launcher"])
     # Run picsar
     
