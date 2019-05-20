7860ca7ca3f1c2572d57f136c201180e07af45f4
Luca Fedeli <luca.fedeli@cea.fr>
2019-05-20 17:30:37 +0200

    new integration strategy

--- src/multi_physics/QED/src/quadrature.hpp
+++ src/multi_physics/QED/src/quadrature.hpp
@@ -1,15 +1,14 @@
 #ifndef __PICSAR_MULTIPHYSICS_QUADRATURE__
 #define __PICSAR_MULTIPHYSICS_QUADRATURE__
 
-//This .hpp file is a wrapper aroud the trapezoidal / exp_sinh method
+//This .hpp file is a wrapper aroud the gauss_kronrod method
 //provided by the Boost library
 
 #include <cmath>
 #include <functional>
 #include <limits>
 
-#include <boost/math/quadrature/trapezoidal.hpp>
-#include <boost/math/quadrature/exp_sinh.hpp>
+#include <boost/math/quadrature/gauss_kronrod.hpp>
 
 //Should be included by all the src files of the library
 #include "qed_commons.h"
@@ -40,7 +39,8 @@ PXRMP_FORCE_INLINE
 _REAL picsar::multi_physics::quad_a_b
 (const std::function<_REAL(_REAL)>& f, _REAL a, _REAL b)
 {
-    return boost::math::quadrature::trapezoidal(f, a, b);
+    return boost::math::quadrature::gauss_kronrod<_REAL, 15>
+        ::integrate(f, a, b);
 }
 
 //This is really just a wrapper around the function provided by
@@ -50,8 +50,8 @@ PXRMP_FORCE_INLINE
 _REAL picsar::multi_physics::quad_a_inf
 (const std::function<_REAL(_REAL)>& f, _REAL a)
 {
-    boost::math::quadrature::exp_sinh<_REAL> integrator;
-    return integrator.integrate(f, a);
+    return boost::math::quadrature::gauss_kronrod<_REAL, 15>
+        ::integrate(f, a, std::numeric_limits<_REAL>::infinity());
 }
 
 #endif //__PICSAR_MULTIPHYSICS_QUADRATURE__
