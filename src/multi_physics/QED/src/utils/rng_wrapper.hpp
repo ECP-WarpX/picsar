43c6a2f81a56c623df9e661e553e6155bcf0fa6a
Luca Fedeli <luca.fedeli.88@gmail.com>
2019-12-17 01:58:58 +0100

    work towards adding back schwinger limit

---
+++ src/multi_physics/QED/src/utils/rng_wrapper.hpp
@@ -0,0 +1,56 @@
+#ifndef PICSAR_MULTIPHYSICS_RNG_WRAPPER
+#define PICSAR_MULTIPHYSICS_RNG_WRAPPER
+
+//This .hpp file provides a wrapper for the RNG.
+//It builds a wrapper around the 64 bit Mersenne Twister availabe in the STL.
+
+#include <random>
+#include <cmath>
+#include <cstdint>
+
+//Should be included by all the src files of the library
+#include "../qed_commons.h"
+
+namespace picsar{
+namespace multi_physics{
+namespace utils{
+
+    //*** STL RNG Wrapper ***
+    class stl_rng_wrapper
+    {
+    public:
+        //The seed must be a 64 bit integer
+        stl_rng_wrapper(uint64_t seed):
+            m_rng.seed{seed}{}
+
+        //Get rnd number uniformly distributed in [a,b)
+        template<typename RealType>
+        PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
+        RealType unf(RealType a, RealType b)
+        {
+            auto unf_dist_a_b =
+                std::uniform_real_distribution<RealType>(a, b);
+            return unf_dist_a_b(m_rng);
+        }
+
+        //Get rnd number with exponential distribution
+        template<typename RealType>
+        PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
+        RealType exp(RealType l)
+        {
+            auto exp_dist_l =
+                std::exponential_distribution<RealType>(l);
+            return exp_dist_l(m_rng);
+        }
+
+    private:
+        //Internally the 64 bit Mersenne Twister generator is used
+        std::mt19937_64 m_rng;
+
+    };
+
+}
+}
+}
+
+#endif //PICSAR_MULTIPHYSICS_RNG_WRAPPER
