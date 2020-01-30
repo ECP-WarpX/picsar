1cbbe93ee96e4013318972c308adcc8b6d278b1b
Luca Fedeli <luca.fedeli@cea.fr>
2020-01-30 14:31:10 +0100

    added comments to picsar_algo

--- src/multi_physics/QED/src/utils/picsar_algo.hpp
+++ src/multi_physics/QED/src/utils/picsar_algo.hpp
@@ -12,6 +12,26 @@ namespace picsar{
 namespace multi_physics{
 namespace utils{
 
+/**
+* This function returns an iterator pointing
+* to the first element in the range [first,last)
+* which compares greater than val. If no element in the range compares
+* greater than val, the function returns last.
+* If the internal preprocessor variable PXRMP_INTERNAL_PICSAR_UPPER_BOUND
+* is defined, a GPU-compatible version is used. Otherwise, if the
+* internal preprocessor variable PXRMP_INTERNAL_STL_UPPER_BOUND is defined,
+* the upper_bound function of the standard template library is used.
+* The choice should be made automatically by the library depending on if
+* the code is compiled for GPU. However, the PICSAR implementation can
+* be forced defining PXRMP_FORCE_PICSAR_UPPER_BOUND.
+*
+* @tparam ForwardIt the iterator type
+* @tparam the type of 'val'
+* @param[in] first a ForwardIt pointing to the first element of the container
+* @param[in] last a ForwardIt pointing to the end of the container (i.e. beyond the last element)
+* @param[in] val the value to use to find the upper bound
+* @return a ForwardIt to the upper bound
+*/
 template<typename ForwardIt, typename T>
 PXRMP_INTERNAL_GPU_DECORATOR
 PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
@@ -19,7 +39,6 @@ ForwardIt
 picsar_upper_bound
 (ForwardIt first, ForwardIt last, const T& val)
 {
-
 #ifdef PXRMP_INTERNAL_PICSAR_UPPER_BOUND
 
     size_t count = last-first;
