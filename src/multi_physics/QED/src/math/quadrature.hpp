246f06bb62f9bdc27e27c7ed8653f85431105fd5
Luca Fedeli <luca.fedeli@cea.fr>
2020-02-04 15:01:06 +0100

    added comments and fixed bugs in quadrature

--- src/multi_physics/QED/src/math/quadrature.hpp
+++ src/multi_physics/QED/src/math/quadrature.hpp
@@ -1,29 +1,31 @@
 #ifndef PICSAR_MULTIPHYSICS_QUADRATURE
 #define PICSAR_MULTIPHYSICS_QUADRATURE
 
-//This .hpp file is a wrapper aroud the trapezoidal, gauss_kronrod & tanh_sinh methods
-//provided by the Boost library
-
 #include <cmath>
 #include <functional>
 #include <limits>
 
 #include <boost/math/quadrature/trapezoidal.hpp>
 #include <boost/math/quadrature/tanh_sinh.hpp>
+#include <boost/math/quadrature/exp_sinh.hpp>
 #include <boost/math/quadrature/gauss_kronrod.hpp>
 
 //Should be included by all the src files of the library
 #include "../qed_commons.h"
 
-//############################################### Declaration
-
 namespace picsar{
 namespace multi_physics{
 namespace math{
-
+    /**
+    * This module is a wrapper is a wrapper aroud the trapezoidal,
+    * gauss_kronrod, tanh_sinh & exp_sinh quadrature methods provided by the Boost library.
+    * All the functions provided here accept a quadrature_algorithm template
+    * parameter to choose which method will be used.
+    */
     enum quadrature_algorithm {
         trapezoidal,
         tanh_sinh,
+        exp_sinh,
         gauss_kronrod15,
         gauss_kronrod31,
         gauss_kronrod41,
@@ -31,7 +33,17 @@ namespace math{
         gauss_kronrod61
     };
 
-    //Generic
+    /**
+    * This function performs the integration of the function f(x)
+    * in the interval (a,b) using the method specified in the template parameter
+    *
+    * @tparam RealType the floating point type to be used
+    * @tparam QuadAlgo the quadrature method to be used
+    * @param[in] f the function which should be integrated
+    * @param[in] a the left boundary of the integration region
+    * @param[in] b the right boundary of the integration region
+    * @return the integral of f in (a,b)
+    */
     template<
         typename RealType, quadrature_algorithm QuadAlgo>
     PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
@@ -43,6 +55,10 @@ namespace math{
         }
         else if(QuadAlgo == quadrature_algorithm::tanh_sinh){
             boost::math::quadrature::tanh_sinh<RealType> integrator;
+            return integrator.integrate(f, a, b);
+        }
+        else if(QuadAlgo == quadrature_algorithm::exp_sinh){
+            boost::math::quadrature::exp_sinh<RealType> integrator;
             return integrator.integrate(f, a, b);
         }
         else if(QuadAlgo == quadrature_algorithm::gauss_kronrod15){
@@ -71,38 +87,60 @@ namespace math{
         }
     }
 
-    //Provides integration in a finite interval [a,b]
-    template<
-        typename RealType,
-        quadrature_algorithm QuadAlgo = quadrature_algorithm::trapezoidal>
+    /**
+    * This function performs the integration of the function f(x)
+    * in the finite interval (a,b), using the "trapezoidal" quadrature method.
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] f the function which should be integrated
+    * @param[in] a the left boundary of the integration region
+    * @param[in] b the right boundary of the integration region
+    * @return the integral of f in (a,b)
+    */
+    template<typename RealType>
     PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     constexpr RealType quad_a_b(
         const std::function<RealType(RealType)>& f, RealType a, RealType b)
     {
-        return generic_quad_a_b<RealType, QuadAlgo>(f, a, b);
+        return generic_quad_a_b<
+            RealType, quadrature_algorithm::trapezoidal>(f, a, b);
     }
 
-    //Provides integration in a finite interval [a,b] (more suitable for
-    //singularities at boundaries )
-    template<
-        typename RealType,
-        quadrature_algorithm QuadAlgo = quadrature_algorithm::tanh_sinh>
+    /**
+    * This function performs the integration of the function f(x)
+    * in the finite interval (a,b) using the "tanh_sinh" quadrature method,
+    * to deal with possibile singularities at the boundaries.
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] f the function which should be integrated
+    * @param[in] a the left boundary of the integration region
+    * @param[in] b the right boundary of the integration region
+    * @return the integral of f in (a,b)
+    */
+    template<typename RealType>
     PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     constexpr RealType quad_a_b_s(
         const std::function<RealType(RealType)>& f, RealType a, RealType b)
     {
-         return generic_quad_a_b<RealType, QuadAlgo>(f, a, b);
+         return generic_quad_a_b<RealType,
+            quadrature_algorithm::tanh_sinh>(f, a, b);
     }
 
-    //Provides integration in a semi-infinite interval [a,inf)
-    template<
-        typename RealType,
-        quadrature_algorithm QuadAlgo = quadrature_algorithm::gauss_kronrod15>
+    /**
+    * This function performs the integration of the function f(x)
+    * in the semi-infinite interval (a,inf) using the "exp_sinh" quadrature method.
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] a the left boundary of the integration region
+    * @param[in] b the right boundary of the integration region
+    * @return the integral of f in (a,b)
+    */
+    template<typename RealType>
     PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     constexpr RealType quad_a_inf(
         const std::function<RealType(RealType)>& f, RealType a)
     {
-        return generic_quad_a_b<RealType, QuadAlgo>(
+        return generic_quad_a_b<RealType, quadrature_algorithm::exp_sinh>(
             f, a, std::numeric_limits<RealType>::infinity());
     }
 }
