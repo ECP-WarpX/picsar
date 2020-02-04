de85dec543a0e4f9a44381f3a9ba16ded77922d8
Luca Fedeli <luca.fedeli@cea.fr>
2020-02-04 11:31:23 +0100

    added comments to vec_functions

--- src/multi_physics/QED/src/math/vec_functions.hpp
+++ src/multi_physics/QED/src/math/vec_functions.hpp
@@ -19,7 +19,13 @@ namespace math{
     template <typename RealType>
     using vec3 = containers::picsar_array<RealType, 3>;
 
-    //Squared norm of a 3-vector
+    /**
+    * This function returns the squared norm of a 3-vector
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] the vector for which the squared norm has to be calculated
+    * @return the squared norm
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     RealType norm2(const vec3<RealType>& vec) noexcept
@@ -27,7 +33,13 @@ namespace math{
         return vec[0]*vec[0]+vec[1]*vec[1]+vec[2]*vec[2];
     }
 
-    //Norm of a 3-vector
+    /**
+    * This function returns the norm of a 3-vector
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] the vector for which the norm has to be calculated
+    * @return the norm
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     RealType norm(const vec3<RealType>& vec) noexcept
@@ -35,7 +47,14 @@ namespace math{
         return sqrt(norm2(vec));
     }
 
-    //Dot product of two 3-vectors
+    /**
+    * This function returns the dot product between two 3-vectors
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] va the first vector
+    * @param[in] vb the second vector
+    * @return the dot product between va and vb
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     RealType dot(const vec3<RealType>& va, const vec3<RealType>& vb) noexcept
@@ -43,7 +62,14 @@ namespace math{
         return va[0]*vb[0] + va[1]*vb[1] + va[2]*vb[2];
     }
 
-    //Cross product of two 3-vectors
+    /**
+    * This function returns the cross product between two 3-vectors
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] va the first vector
+    * @param[in] vb the second vector
+    * @return the cross product between va and vb
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> cross(const vec3<RealType>& va, const vec3<RealType>& vb) noexcept
@@ -55,7 +81,14 @@ namespace math{
         return out;
     }
 
-    //Product of a vector times a scalar
+    /**
+    * This function overloads the * operator for a vector and a scalar
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] v the vector
+    * @param[in] s the scalar
+    * @return the scalar product between v and s
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> operator*(const vec3<RealType>& v, const RealType s) noexcept
@@ -67,7 +100,14 @@ namespace math{
         return out;
     }
 
-    //Product of a scalar times a vector
+    /**
+    * This function overloads the * operator for a scalar and a vector
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] s the scalar
+    * @param[in] v the vector
+    * @return the scalar product between s and v
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> operator*(const RealType s, const vec3<RealType>& v) noexcept
@@ -79,7 +119,14 @@ namespace math{
         return out;
     }
 
-    //Vector divided by a scalar
+    /**
+    * This function overloads the / operator for a vector and a scalar
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] v the vector
+    * @param[in] s the scalar
+    * @return v/s
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> operator/(const vec3<RealType>& v, const RealType s) noexcept
@@ -91,7 +138,14 @@ namespace math{
         return out;
     }
 
-    //Add two vectors
+    /**
+    * This function overloads the + operator for two vectors
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] va the first vector
+    * @param[in] vb the second vector
+    * @return va+vb
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> operator+(const vec3<RealType>& va, const vec3<RealType>& vb) noexcept
@@ -103,7 +157,14 @@ namespace math{
         return out;
     }
 
-    //Subtract two vectors
+    /**
+    * This function overloads the - operator for two vectors
+    *
+    * @tparam RealType the floating point type to be used
+    * @param[in] va the first vector
+    * @param[in] vb the second vector
+    * @return va-vb
+    */
     template <typename RealType>
     PXRMP_INTERNAL_GPU_DECORATOR PXRMP_INTERNAL_FORCE_INLINE_DECORATOR
     vec3<RealType> operator-(const vec3<RealType>& va, const vec3<RealType>& vb) noexcept
