a93dccc295461cbc2a6adb5942f45b6a100f17da
Luca Fedeli <luca.fedeli.88@gmail.com>
2020-07-17 16:27:49 +0200

    Update src/multi_physics/QED/src/physics/quantum_sync/quantum_sync_engine_tables.hpp
    Co-authored-by: NeilZaim <49716072+NeilZaim@users.noreply.github.com>

--- src/multi_physics/QED/src/physics/quantum_sync/quantum_sync_engine_tables.hpp
+++ src/multi_physics/QED/src/physics/quantum_sync/quantum_sync_engine_tables.hpp
@@ -439,7 +439,7 @@ namespace quantum_sync{
     * (see validation script) and providing methods to perform interpolations.
     * It also provides methods for serialization (export to byte array,
     * import from byte array) and to generate "table views" based on
-    * non-owning poynters (this is crucial in order to use the table
+    * non-owning pointers (this is crucial in order to use the table
     * in GPU kernels, as explained below).
     *
     * Internally, this table stores
