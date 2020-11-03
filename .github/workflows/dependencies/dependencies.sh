82c314660c7632e1503f3aba42b1046d3d0d9502
Axel Huebl <axel.huebl@plasma.ninja>
2020-11-03 10:21:05 -0800

    Multi-Physics: Add CI

---
+++ .github/workflows/dependencies/dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 The PICSAR Community
+#
+# License: BSD-3-Clause-LBNL
+# Authors: Axel Huebl
+
+set -eu -o pipefail
+
+sudo apt-get update
+
+sudo apt-get install -y --no-install-recommends\
+    build-essential \
+    cmake           \
+    libboost-dev    \
+    g++ gfortran
