a1c3b1255dea8ef1b11309d39c696b6540aebab2
Luca Fedeli <luca.fedeli@cea.fr>
2021-04-21 10:35:21 +0200

    BW tables with Kokkos vector

--- multi_physics/QED/kokkos_example/kokkos_example.cpp
+++ multi_physics/QED/kokkos_example/kokkos_example.cpp
@@ -1,15 +1,16 @@
-
 #include <Kokkos_Core.hpp>
-
-#include <iostream>
+#include <Kokkos_Vector.hpp>
 
 // PICSAR MULTIPHYSICS: BREIT WHEELER ENGINE
+#define PXRMP_FORCE_INLINE
 #define PXRMP_WITH_GPU
-#define PXRMP_GPU_QUALIFIER
+#define PXRMP_GPU_QUALIFIER KOKKOS_INLINE_FUNCTION
 #include <picsar_qed/physics/breit_wheeler/breit_wheeler_engine_core.hpp>
 #include <picsar_qed/physics/breit_wheeler/breit_wheeler_engine_tables.hpp>
 #include <picsar_qed/physics/breit_wheeler/breit_wheeler_engine_tables_generator.hpp>
 //__________________________________________________
+
+#include <iostream>
 
 //Some namespace aliases
 namespace pxr =  picsar::multi_physics::phys;
@@ -28,8 +29,19 @@ const double Es = pxr::schwinger_field<>;
 const double Bs = pxr::schwinger_field<>/pxr::light_speed<>;
 //__________________________________________________
 
-const int HOW_MANY_PARTICLES = 10'000'000;
-const int HOW_MANY_REPETITIONS = 10;
+//Parameters of the test case
+const int how_many_particles = 10'000'000;
+const int how_many_repetitions = 10;
+const double dt_test = 1e-18;
+const double table_chi_min = 0.01;
+const double table_chi_max = 1000.0;
+const int table_chi_size = 256;
+const int table_frac_size = 256;
+const double max_normalized_field = 0.02;
+const double max_normalized_momentum = 1000.0;
+const int random_seed = 22051988;
+//__________________________________________________
+
 
 template<typename Real>
 struct particle_data{
@@ -63,10 +75,60 @@ particle_data<Real> create_particles(int how_many)
     return pdata;
 }
 
+
+template <typename Real, typename Vector>
+auto generate_dndt_table(Real chi_min, Real chi_max, int chi_size)
+{
+    std::cout << "Preparing dndt table [" << typeid(Real).name() << ", " << chi_size <<"]...\n";
+    std::cout.flush();
+
+    pxr_bw::dndt_lookup_table_params<Real> bw_params{chi_min, chi_max, chi_size};
+
+	auto table = pxr_bw::dndt_lookup_table<
+        Real, Vector>{bw_params};
+
+    table.generate();
+
+    return table;
+}
+
+template <typename Real, typename Vector>
+auto generate_pair_table(Real chi_min, Real chi_max, int chi_size, int frac_size)
+{
+    std::cout << "Preparing pair production table [" << typeid(Real).name() << ", " << chi_size << " x " << frac_size <<"]...\n";
+    std::cout.flush();
+
+    pxr_bw::pair_prod_lookup_table_params<Real> bw_params{
+        chi_min, chi_max, chi_size, frac_size};
+
+	auto table = pxr_bw::pair_prod_lookup_table<
+        Real, Vector>{bw_params};
+
+    table.template generate();
+
+    return table;
+}
+
 template <typename Real>
 void do_test()
 {
     particle_data<Real> particle_data;
+
+    const auto dndt_table =
+        generate_dndt_table<Real, Kokkos::vector<Real>>(
+            table_chi_min,
+            table_chi_max,
+            table_chi_size);
+
+    const auto pair_table =
+        generate_pair_table<Real,Kokkos::vector<Real>>(
+            table_chi_min,
+            table_chi_max,
+            table_chi_size,
+            table_frac_size);
+
+    const auto dndt_table_view = dndt_table.get_view();
+    const auto pair_table_view = pair_table.get_view();
 }
 
 
