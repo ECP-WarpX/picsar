a1e1b4f41c4ad5b997db5edf14a1673a2d9fb154
Luca Fedeli <luca.fedeli@cea.fr>
2021-10-26 15:27:09 +0200

    fix revert

---
+++ .github/workflows/dependencies/dependencies_mac.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 The PICSAR Community
+#
+# License: BSD-3-Clause-LBNL
+# Authors: Axel Huebl, Luca Fedeli
+
+set -eu -o pipefail
+
+brew update
+brew install boost
+brew install pybind11
+brew install libomp
+brew install llvm
+#brew install open-mpi
