27b6a51ec46fe6fb5ed73cc4f36c189a1dc86f1a
Axel Huebl <axel.huebl@plasma.ninja>
2024-08-18 15:40:14 -0700

    SYCL Update Namespaces too
    modernize

--- multi_physics/QED/include/picsar_qed/math/cmath_overloads.hpp
+++ multi_physics/QED/include/picsar_qed/math/cmath_overloads.hpp
@@ -205,7 +205,7 @@ namespace math{
     float m_floor(const float x) noexcept
     {
 #ifdef PXRMP_DPCPP_FIX
-        return cl::sycl::floorf(x);
+        return sycl::floorf(x);
 #else
         return floorf(x);
 #endif
@@ -331,7 +331,7 @@ namespace math{
     RealType m_floor(const RealType x) noexcept
     {
 #ifdef PXRMP_DPCPP_FIX
-        return cl::sycl::floor(x);
+        return sycl::floor(x);
 #else
         return std::floor(x);
 #endif
