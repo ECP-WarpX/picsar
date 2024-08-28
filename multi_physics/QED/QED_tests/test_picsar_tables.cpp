4e31f7c7a30a58ec43857ddb1a9f1d9e95f5244c
Axel Huebl <axel.huebl@plasma.ninja>
2024-08-28 16:00:43 -0700

    `test_picsar_tables.cpp`: Bump Tolerance 5x

--- multi_physics/QED/QED_tests/test_picsar_tables.cpp
+++ multi_physics/QED/QED_tests/test_picsar_tables.cpp
@@ -20,10 +20,10 @@ using namespace picsar::multi_physics::containers;
 namespace pxr_ser = picsar::multi_physics::utils::serialization;
 
 //Tolerance for double precision calculations
-const double double_tolerance = 2.0e-12;
+const double double_tolerance = 5.0e-12;
 
 //Tolerance for single precision calculations
-const float float_tolerance = 2.0e-5;
+const float float_tolerance = 5.0e-5;
 
 //Templated tolerance
 template <typename T>
