a6b1c57654606b0f078b6887af1b4b474ddbbb2b
Luca Fedeli <luca.fedeli@cea.fr>
2026-04-28 10:36:46 +0200

    pass by copy

--- multi_physics/QED/python_bindings/pxr_qed.cpp
+++ multi_physics/QED/python_bindings/pxr_qed.cpp
@@ -280,7 +280,7 @@ compute_gamma_photon_wrapper(
     auto res = pyArr(how_many);
     auto* p_res = static_cast<REAL*>(res.request().ptr);
 
-    PXRQEDPY_FOR(static_cast<int>(how_many), [&, p_px=p_px, p_py=p_py, p_pz=p_pz](int i){
+    PXRQEDPY_FOR(static_cast<int>(how_many), [=](int i){
         p_res[i] =
             pxr_phys::compute_gamma_photon<REAL, UU>(
                 p_px[i], p_py[i], p_pz[i],
